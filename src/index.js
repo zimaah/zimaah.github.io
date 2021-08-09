ebefb32958383adb75c6a93e5c2e08345b183dd3
Guilherme Zimmermann <guilherme@simply-vc.com.mt>
2021-08-09 16:02:14 +0200

    Removed console.log + added initial-scale=1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import * as Components from './components.js';
-console.log(Components);
 
 /**
  * We need to register our custom elements before start using them.
  */
  (function(){
-     console.log(window);
      window.customElements.define('zimaah-card', Components.CardComponent);
 })();
 
